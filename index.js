f5c179a6621469df16a12e8f56921624fe118c05
Harish Kumar Nagarajan <cnharishnagaraj@gmail.com>
2024-05-06 20:06:20 +0530

    create API server for todo

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!")
+const express = require('express')
+
+const api = express()
+
+api.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+api.listen(2323, ()=>{
+    console.log("Hello world!")
+    console.log("API Server is up and running in port: 2323");
+})
