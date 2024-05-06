925bd57879e7ad887f53676ab3cc8ed71d4bf065
Harish Kumar Nagarajan <cnharishnagaraj@gmail.com>
2024-05-06 20:25:55 +0530

    Add MongoDb connection

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose');
 
 const api = express()
 
+// Connnect to MongoDb
+mongoose.connect('mongodb+srv://harish:root@cnhk-teach-db.zpoered.mongodb.net/todo')
+    .then(() => {
+        console.log("Connected to MongoDB");
+        // Start your server after successful MongoDB connection
+        api.listen(2323, ()=>{
+            console.log("Hello world!")
+            console.log("API Server is up and running in port: 2323");
+        })
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+
+
 api.get('/', (req, res) => {
     res.send('Hello world!')
-})
-
-api.listen(2323, ()=>{
-    console.log("Hello world!")
-    console.log("API Server is up and running in port: 2323");
 })
