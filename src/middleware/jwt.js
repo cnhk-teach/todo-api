77c362f7cedb1289efa6d87cd4393ac72e99dec3
Harish Kumar Nagarajan <cnharishnagaraj@gmail.com>
2024-05-06 23:51:23 +0530

    feat. add jwt middleware to verifyUser based on jwt token

---
+++ src/middleware/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const SECRET_KEY = 'yuyughrbgekbgjkebjldsfdjbsdbvlsdhsgdhbdsbfbvdhvj'
+
+function verifyUser(req, res, next) {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        if (!token) {
+            return res.status(401).json({ err: 'Access denies, no token' })
+        }
+
+        jwt.verify(token, SECRET_KEY, (err, data)=>{
+            if(err){
+                return res.status(403).json({ err: 'Access denied' })
+            }
+            req.user = data;
+            next()
+        })
+    } catch (error) {
+        return res.status(500).json({ err: 'Internal error in JWT verification' })
+    }
+    
+}
+
+module.exports = verifyUser
--- src/routes/todoAction.js
+++ src/routes/todoAction.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const Todo = require('../models/Todo')
+const verifyUser =  require('../middleware/jwt')
 const todoAction = express.Router()
 
-todoAction.get('/view', (_req, res) => {
+todoAction.get('/view', verifyUser, (req, res) => {
     try {
-        Todo.find()
+        var userId = req.user.userId
+        Todo.find({ userId: userId })
             .then((todos) => res.status(200).json(todos))
             .catch((err) => res.status(400).json({err: err}))
     } catch (err) {
