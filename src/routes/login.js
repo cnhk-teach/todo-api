ef7dcc5050a4b351b75502270a9c368f2b2bf2d3
Harish Kumar Nagarajan <cnharishnagaraj@gmail.com>
2024-05-06 23:28:07 +0530

    Feat. generate token for logged in users

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,18 +2,22 @@ const express = require('express')
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const login = express.Router()
+const jwt = require('jsonwebtoken')
+
+const SECRET_KEY = 'yuyughrbgekbgjkebjldsfdjbsdbvlsdhsgdhbdsbfbvdhvj'
 
 login.post('/login', async (req, res) => {
     try {
         const validUser = await User.findOne({email: req.body.email})
         if (!validUser) {
-            return res.status(404).send('User not exist')
+            return res.status(404).json({msg: 'User not exist'})
         }
         const isPasswordValid = await bcrypt.compare(req.body.password, validUser.password)
         if(!isPasswordValid){
-            return res.status(400).send('Invalid password')
+            return res.status(400).json({msg: 'Invalid password'})
         }
-        res.status(200).json({msg: `User: ${validUser.username} logged in successfully`})
+        const token = jwt.sign({userId: validUser.id, username: req.body.username}, SECRET_KEY)
+        res.status(200).json({msg: `User: ${validUser.username} logged in successfully`, token: token})
     } catch (err) {
         res.status(500).json({err: 'Internal server error: '+ err })
     }
