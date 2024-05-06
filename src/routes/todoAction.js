ff1a0486de2a0924e339240048c8aa4fffd7e99c
Harish Kumar Nagarajan <cnharishnagaraj@gmail.com>
2024-05-07 00:44:05 +0530

    feat. API to mark todo item as completed

--- src/routes/todoAction.js
+++ src/routes/todoAction.js
@@ -33,4 +33,29 @@ todoAction.post('/add', verifyUser, (req, res) => {
     }
 })
 
+todoAction.patch('/completed/:todoId', verifyUser, async (req, res) => {
+    try {
+        var todoId = req.params.todoId
+        var userTodos = await Todo.find({ userId: req.user.userId})
+        if(userTodos.length == 0) {
+            return res.status(404).json({msg: 'Todo list is empty for this user'})
+        }
+        console.log(userTodos,"user");
+        var validTodo = userTodos.filter((item) => item.id == todoId)
+        if(validTodo.length == 0) {
+            return res.status(401).json({msg: 'Todo id mentioned is not valid to be modified by you'})
+        }
+        console.log(validTodo)
+        Todo.findByIdAndUpdate(validTodo[0].id, { completedAt: Date.now()})
+            .then(() => 
+                res.status(200).json({msg: "Todo item completed!"})
+            )
+            .catch((err) => 
+                res.status(400).json({err: `Todo item not updated, error: ${err}`})
+            )
+    } catch (err) {
+        res.status(500).json({err: 'Internal server error: '+ err })
+    }
+})
+
 module.exports = todoAction
