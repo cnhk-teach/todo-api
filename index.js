86aac453d2969fafd1a2eaf62cc27740b2d37066
Harish Kumar Nagarajan <cnharishnagaraj@gmail.com>
2024-05-08 01:28:55 +0530

    enable CORS for local angular project

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'http://localhost:4200',
+   optionSuccessStatus:200,
+}
+
 const signup = require('./src/routes/signup')
 const login = require('./src/routes/login')
 const todoAction = require('./src/routes/todoAction')
 
 const api = express()
 api.use(express.json())
+api.use(cors(corsOptions))
+
 const PORT = process.env.PORT || 3000;
 
 // Connnect to MongoDb
